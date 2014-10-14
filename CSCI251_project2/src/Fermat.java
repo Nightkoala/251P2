cbde11a057e1551e670cc917d20bd33b6333ccaf
Derek Brown <djb3718@rit.edu>
2014-10-14 15:53:28 -0400

    Added to Eclipse and create java file

---
+++ CSCI251_project2/src/Fermat.java
@@ -0,0 +1,15 @@
+/**
+ * Fermat.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose	Calculate the number of witnesses a number has using Fermat's
+ * 			Little Theorem,  Gain experience using the PJ2 library.
+ */
+
+public class Fermat {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end Fermat
