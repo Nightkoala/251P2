3d67548dc48e1ded858601163856e6d558b9ff97
Derek Brown <djb3718@rit.edu>
2014-10-15 14:32:34 -0400

    Added javadoc comments

--- CSCI251_project2/src/Fermat.java
+++ CSCI251_project2/src/Fermat.java
@@ -12,8 +12,23 @@ import edu.rit.pj2.*;
 
 public class Fermat extends Task {
 
+	/**
+	 * Declaration of shared variable for counting the number of witnesses.
+	 */
 	IntVbl numWitness;
 	
+	/**
+	 * Helper function that determines if a value is a witness of p using 
+	 * Fermat's Little Theorem.
+	 * 
+	 * @param p		The value that we are trying to find the number of 
+	 * 				witnesses for.
+	 * @param a		The current value that we are checking to see if it is a 
+	 * 				witness of p.
+	 * 
+	 * @return		true, if a is a witness of p
+	 * 				false, otherwise
+	 */
 	public static boolean isWitness( BigInteger p, BigInteger a ) {
 		if( a.modPow(p, p).compareTo(a) != 0 ) {
 			return true;
@@ -21,6 +36,18 @@ public class Fermat extends Task {
 		return false;
 	}//end isWitness
 
+	/**
+	 * Main method,  sterilizes the input and validates that the user input
+	 * is of the correct form,  The program then finds the number of witnesses
+	 * for p (stored in the shared numWitness variable) using parallel
+	 * structures,  The program then displays the number of witnesses.
+	 * 
+	 * @param args		args[0] contains the value of p that we are attempting
+	 * 					to find the number of witnesses for.
+	 * 
+	 * @exception NumberFormatException	Thrown if the value given by user is not
+	 * 									an integer.
+	 */
 	public void main( final String[] args ) {
 		if( args.length != 1 ) {
 			System.err.println("Usage: java pj2 Fermat <p>");
