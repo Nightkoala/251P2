2cd1ebf6ae6768deec8f8e2bae94e816638d5e62
Nightkoala <djb3718@rit.edu>
2014-10-14 21:31:40 -0400

    finished parallel functionality.  Still need to clean up code and add javadocs

--- CSCI251_project2/src/Fermat.java
+++ CSCI251_project2/src/Fermat.java
@@ -4,33 +4,24 @@
  * @author	Derek Brown <djb3718@rit.edu>
  *
  * Purpose	Calculate the number of witnesses a number has using Fermat's
- * 			Little Theorem,  Gain experience using the PJ2 library.
+ * 		Little Theorem,  Gain experience using the PJ2 library.
  */
 
 import java.math.BigInteger;
+import edu.rit.pj2.*;
 
-public class Fermat {
+public class Fermat extends Task {
+
+	IntVbl numWitness;
 	
-	// Attributes
-	private BigInteger p;
-	private BigInteger numWitness;
-	
-	// Constructor
-	public Fermat( int value ) {
-		p = BigInteger.valueOf(value);
-		numWitness = BigInteger.valueOf(0);
-	}//end Fermat constructor
-	
-	// Methods
-	
-	public boolean isWitness( BigInteger p, BigInteger a ) {
+	public static boolean isWitness( BigInteger p, BigInteger a ) {
 		if( a.modPow(p, p).compareTo(a) != 0 ) {
 			return true;
 		}//end if
 		return false;
 	}//end isWitness
 
-	public static void main( String[] args ) {
+	public void main( final String[] args ) {
 		if( args.length != 1 ) {
 			System.err.println("Usage: java pj2 Fermat <p>");
 			System.exit(1);
@@ -40,24 +31,34 @@ public class Fermat {
 		try {
 			input = Integer.parseInt( args[0] );
 		} catch( NumberFormatException e ) {
-			System.err.println( args[0] + " not an integer. Abort." );
+			System.err.println( args[0] +
+				" not an integer. Abort." );
 			e.printStackTrace( System.err );
 			System.exit( 1 );
 		}//end try/catch
 		input = Integer.parseInt( args[0] );
 		
 		if( input < 3 ) {
-			System.err.println("<p> must be greater than or equal to 3");
+			System.err.println(
+				"<p> must be greater than or equal to 3");
+			System.exit(1);
 		}//end if
 		
-		Fermat F = new Fermat( input );
-
-		for( int i = 2 ; i < F.p.intValue() ; i++ ) {
-			BigInteger a = BigInteger.valueOf(i);
-			if( F.isWitness( F.p, a ) ) {
-				F.numWitness = F.numWitness.add(BigInteger.valueOf(1));
-			}//end if
-		}//end for
-		System.out.println(F.numWitness);
+		numWitness = new IntVbl.Sum(0);
+		parallelFor( 2, input-1 ).exec( new Loop() {
+			IntVbl thrNumWitness;
+			public void start() {
+				thrNumWitness = threadLocal( numWitness );
+			}//end start
+			public void run( int i ) {
+				int val = Integer.parseInt( args[0] );
+				BigInteger p = BigInteger.valueOf( val );
+				BigInteger a = BigInteger.valueOf(i);
+				if( isWitness( p, a ) ) {
+					++thrNumWitness.item;
+				}//end if
+			}//end run
+		});//end for
+		System.out.println(numWitness.item);
 	}//end main
 }//end Fermat
